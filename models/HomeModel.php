4280f15517dca75bf649826135f0cf67aeed5a9b
TranNgocTam <ngoctam2303001@gmail.com>
2021-11-19 13:59:12 +0700

    Update delete

---
+++ models/HomeModel.php
@@ -0,0 +1,27 @@
+<?php
+require_once 'BaseModel.php'; 
+class HomeModel extends BaseModel {
+      //Login
+      public function login($email, $password)
+      {
+          $md5Password = md5($password);
+          $sql = 'SELECT * FROM users WHERE email = "' . $email . '" AND password = "' . $md5Password . '"';
+  
+          $user = $this->select($sql);
+          return $user;
+      }
+  
+      //Register
+      public function createNewUser($input)
+      {
+          $sql = "INSERT INTO `users`(`username`, `email`, `password`,`permission`) 
+          VALUES ('" . $input['username'] . "','" . $input['email'] . "','" . md5($input['password']) . "','" . 'User' . "')";
+  
+          $user = $this->insert($sql);
+  
+          return $user;
+      }
+      //Google
+  
+      //Forget password
+}
--- models/UserModel.php
+++ models/UserModel.php
@@ -1,12 +1,37 @@
 <?php
-require_once 'BaseModel.php'; 
-class UserModel extends BaseModel {
-    //Login
+require_once 'BaseAdminModel.php'; 
+class UserModel extends BaseAdminModel {
+    
+    // Lay danh sach: 
+    public function getUsers($params = [])
+    {
+        //Keyword
+        if (!empty($params['keyword'])) {
+            $sql = 'SELECT * FROM users 
+            WHERE name LIKE "%' . mysqli_real_escape_string(self::$_connection, $params['keyword']) . '%"';
+            //Keep this line to use Sql Injection
+            //Don't change
+            //Example keyword: abcef%";TRUNCATE banks;##
+            //$users = self::$_connection->multi_query($sql);
+            $users = $this->select($sql);
+        } else {
+            $sql = 'SELECT * FROM users';
+            $users = $this->select($sql);
+        }
 
-    //Register
-
-    //Google
-
-    //Forget password
+        return $users;
+    }
+    //Xoa người dùng: 
+    public function deleteUserById($id)
+    {
+        $sql = 'DELETE FROM users WHERE id = ' . $id;
+        return $this->delete($sql);
+    }
+    //Tìm id 
+    public function findUserById($id) {
+        $sql = 'SELECT * FROM users WHERE id = '.$id;
+        $user = $this->select($sql);
+        return $user;
+    }
     
 }
