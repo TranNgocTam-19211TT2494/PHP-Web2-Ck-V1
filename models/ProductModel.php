ac191c55265086a0b3ef93272188e753d8546d99
ngo thanh thai <ngothai653@gmail.com>
2021-11-20 17:11:02 +0700

    Delete Product

--- models/ProductModel.php
+++ models/ProductModel.php
@@ -1,13 +1,92 @@
 <?php
 
-require_once 'BaseModel.php';
+require_once 'BaseAdminModel.php';
 
-class ProductModel extends BaseModel
+class ProductModel extends BaseAdminModel
 {
-    public function getProducts()
+    public function insertProduct($input)
     {
-        $sql = 'SELECT * FROM products';
+        if (isset($input) && is_array($input)) {
+                $url = 'http://localhost/PHP-Web2-Ck-V1.git/public/img/product/';
+                $name = $url . $_FILES["image"]['name'];
+                $sql = "INSERT INTO `webbanhkem`.`products` (`name`, `manu_id` ,`type_id`,`price`, `pro_image`,`feature`,`description`  ) VALUES (" .
+                    "'" . $input['name'] . "','" . $input['manufacture'] . "','" . $input['protype'] . "','" . $input['price'] 
+                    . "','" . $name
+                    . "','" . $input['feature']
+                    . "','" . $input['description'] . "')";
+                $bank = $this->insert($sql);
+                return $bank;
+        }
+        return false;
+    }
+    public function trashProduct($id)
+    {
+        date_default_timezone_set("Asia/Ho_Chi_Minh");
+        $allProduct = $this->getProducts();
+        $now =  strftime('%Y-%m-%d');
+        foreach ($allProduct as $value) {
+            $md5 = md5($value['id'] . "chuyen-de-web-2");
+            if($md5 == $id){
+                $sql = "UPDATE `products` SET `detele_at`= CURTIME() WHERE id = " . $value['id'] ;
+                $bank = $this->update($sql);
+                return $bank;
+             }
+        }
+      return false;
+    }
+    public function deleteProduct($id)
+    {
+        $allProduct = $this->getAllProducts();
+        foreach ($allProduct as $value) {
+            $md5 = md5($value['id'] . "chuyen-de-web-2");
+            if($md5 == $id){
+                $sql = "DELETE FROM products WHERE id =  " . $value['id'] ;
+                $bank = $this->delete($sql);
+                return $bank;
+             }
+        }
+      return false;
+    }
+    public function getAllTrashProduct()
+    {
+        $sql = 'SELECT * FROM `products` WHERE detele_at IS NOT NULL ORDER BY `id` DESC;';
         $products = $this->select($sql);
         return $products;
+    }
+    public function getProducts()
+    {
+        $sql = 'SELECT * FROM `products` WHERE detele_at IS NULL ORDER BY `id` DESC;';
+        $products = $this->select($sql);
+        return $products;
+    }
+    public function getAllProducts()
+    {
+        $sql = 'SELECT * FROM `products` ORDER BY `id` DESC;';
+        $products = $this->select($sql);
+        return $products;
+    }
+    public function getManufacture()
+    {
+        $sql = 'SELECT * FROM manufactures  ORDER BY `manu_id` DESC';
+        $manufactures = $this->select($sql);
+        return $manufactures;
+    }
+    public function getProtypes()
+    {
+        $sql = 'SELECT * FROM protypes  ORDER BY `type_id` DESC';
+        $protypes = $this->select($sql);
+        return $protypes;
+    }
+    public function getManuByProductId($id)
+    {
+        $sql = 'SELECT manu_name FROM manufactures WHERE manu_id = '.$id;
+        $manu = $this->select($sql);
+        return $manu;
+    }
+    public function getProTypeByProductId($id)
+    {
+        $sql = 'SELECT type_name FROM protypes WHERE type_id = '.$id;
+        $manu = $this->select($sql);
+        return $manu;
     }
 }
---
+++ register.php
@@ -0,0 +1,145 @@
+<?php 
+    require_once('models/UserModel.php');
+    $UserModel = new UserModel();
+
+    if(!empty($_POST['submit'])) {
+
+      
+        if($_POST['username'] != '' && $_POST['email'] != '' && $_POST['password'] != '') {
+            $insert = $UserModel->createNewUser($_POST);
+            if($insert) {
+                header("location: index.php");
+            } else {
+                echo "<div class=\"alert alert-dark\" role=\"alert\">
+                username error or pass not!</div>";
+            }
+        } else {
+            echo "<div class=\"alert alert-dark\" role=\"alert\">Trường phải đủ</div>";
+        }
+      
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="au theme template">
+    <meta name="author" content="Hau Nguyen">
+    <meta name="keywords" content="au theme template">
+
+    <!-- Title Page-->
+    <title>Register</title>
+
+    <!-- Fontfaces CSS-->
+    <link href="public/backend/css/font-face.css" rel="stylesheet" media="all">
+    <link href="public/backend/vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+    <link href="public/backend/vendor/font-awesome-5/css/fontawesome-all.min.css" rel="stylesheet" media="all">
+    <link href="public/backend/vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+
+    <!-- Bootstrap CSS-->
+    <link href="public/backend/vendor/bootstrap-4.1/bootstrap.min.css" rel="stylesheet" media="all">
+
+    <!-- Vendor CSS-->
+    <link href="public/backend/vendor/animsition/animsition.min.css" rel="stylesheet" media="all">
+    <link href="public/backend/vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet"
+        media="all">
+    <link href="public/backend/vendor/wow/animate.css" rel="stylesheet" media="all">
+    <link href="public/backend/vendor/css-hamburgers/hamburgers.min.css" rel="stylesheet" media="all">
+    <link href="public/backend/vendor/slick/slick.css" rel="stylesheet" media="all">
+    <link href="public/backend/vendor/select2/select2.min.css" rel="stylesheet" media="all">
+    <link href="public/backend/vendor/perfect-scrollbar/perfect-scrollbar.css" rel="stylesheet" media="all">
+
+    <!-- Main CSS-->
+    <link href="public/backend/css/theme.css" rel="stylesheet" media="all">
+    
+</head>
+
+<body class="animsition">
+    <div class="page-wrapper">
+        <div class="page-content--bge5">
+            <div class="container">
+                <div class="login-wrap">
+
+                    <div class="login-content">
+                        <div class="login-logo">
+                            <a href="#">
+                                <img src="public/backend/images/icon/logo.png" alt="CoolAdmin">
+                            </a>
+                        </div>
+                        <div class="login-form">
+                            <form method="post">
+                                <div class="form-group">
+                                    <label>Username</label>
+                                    <input class="au-input au-input--full" type="text" name="username"
+                                        placeholder="Username">
+                                </div>
+                                <div class="form-group">
+                                    <label>Email Address</label>
+                                    <input class="au-input au-input--full" type="email" name="email"
+                                        placeholder="Email">
+                                </div>
+                                <div class="form-group">
+                                    <label>Password</label>
+                                    <input class="au-input au-input--full" type="password" name="password"
+                                        placeholder="Password">
+                                </div>
+                                <div class="login-checkbox">
+                                    <label>
+                                        <input type="checkbox" name="aggree">Agree the terms and policy
+                                    </label>
+                                </div>
+                                <button class="au-btn au-btn--block au-btn--green m-b-20" type="submit" name="submit"
+                                    value="submit">register</button>
+                                <div class="social-login-content">
+                                    <div class="social-button">
+                                        <button class="au-btn au-btn--block au-btn--blue m-b-20">register with
+                                            google</button>
+
+                                    </div>
+                                </div>
+                            </form>
+                            <div class="register-link">
+                                <p>
+                                    Already have account?
+                                    <a href="login.php">Sign In</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- Jquery JS-->
+    <script src="public/backend/vendor/jquery-3.2.1.min.js"></script>
+    <!-- Bootstrap JS-->
+    <script src="public/backend/vendor/bootstrap-4.1/popper.min.js"></script>
+    <script src="public/backend/vendor/bootstrap-4.1/bootstrap.min.js"></script>
+    <!-- Vendor JS       -->
+    <script src="public/backend/vendor/slick/slick.min.js">
+    </script>
+    <script src="public/backend/vendor/wow/wow.min.js"></script>
+    <script src="public/backend/vendor/animsition/animsition.min.js"></script>
+    <script src="public/backend/vendor/bootstrap-progressbar/bootstrap-progressbar.min.js">
+    </script>
+    <script src="public/backend/vendor/counter-up/jquery.waypoints.min.js"></script>
+    <script src="public/backend/vendor/counter-up/jquery.counterup.min.js">
+    </script>
+    <script src="public/backend/vendor/circle-progress/circle-progress.min.js"></script>
+    <script src="public/backend/vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
+    <script src="public/backend/vendor/chartjs/Chart.bundle.min.js"></script>
+    <script src="public/backend/vendor/select2/select2.min.js">
+    </script>
+
+    <!-- Main JS-->
+    <script src="public/backend/js/main.js"></script>
+
+</body>
+
+</html>
+<!-- end document-->
