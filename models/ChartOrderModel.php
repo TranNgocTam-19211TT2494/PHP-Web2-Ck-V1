0fd6ea95db6153a0adc6105b94070979ab0b9f62
ngo thanh thai <ngothai653@gmail.com>
2021-11-30 10:21:24 +0700

    Update Chart Order

---
+++ models/ChartOrderModel.php
@@ -0,0 +1,13 @@
+<?php
+require_once "../models/BaseAdminModel.php";
+class ChartOrderModel extends BaseAdminModel
+{
+    public function getAllOrderByMonth($month)
+    {
+        $sql = "SELECT * 
+        FROM checkouts  
+        WHERE MONTH(addedDate) = $month;";
+        $id = $this->select($sql);
+        return $id;
+    }
+}
